c0bf6f9c4a09c34880ba107bfc03eb5537bcc524
Yezato10 <cristianyesid417@gmail.com>
2025-03-10 19:25:32 -0500

    Update Libro.java

--- exercise/src/main/java/co/edu/etitc/sistemas/tecnologo/programacion4/Libro.java
+++ exercise/src/main/java/co/edu/etitc/sistemas/tecnologo/programacion4/Libro.java
@@ -2,7 +2,6 @@ package co.edu.etitc.sistemas.tecnologo.programacion4;
 
 import java.time.LocalDateTime;
 
-
 public class Libro extends Recurso {
     private String autor;
     private String editorial;
@@ -10,36 +9,45 @@ public class Libro extends Recurso {
 
     public Libro(String titulo, LocalDateTime fechaIngreso, boolean activo,
                  String autor, String editorial, int año) {
-
         super(titulo, fechaIngreso, activo);
         this.autor = autor;
         this.editorial = editorial;
         this.año = año;
     }
 
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public int getAño() {
+        return año;
+    }
 
     @Override
     public boolean coincideConCriterio(String criterio) {
+        if (super.coincideConCriterio(criterio)) {
+            return true;
+        }
+
         if (criterio == null || criterio.isEmpty()) {
             return false;
         }
 
-        if (titulo.contains(criterio)
-            || autor.contains(criterio)
+        return autor.contains(criterio)
             || editorial.contains(criterio)
-            || String.valueOf(año).contains(criterio)) {
-            return true;
-        }
-        return false;
+            || String.valueOf(año).contains(criterio);
     }
-
 
     @Override
     public String toString() {
-  
         return super.toString()
             + "autor=" + autor + "\n"
             + "editorial=" + editorial + "\n"
             + "año=" + año + "\n";
     }
 }
+
